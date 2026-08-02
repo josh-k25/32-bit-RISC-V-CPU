3c8e410959c1f42e60365b2167c1e3cf200391f3
josh k <joshuadanielkung@gmail.com>
2026-08-02 14:34:08 +0000

    Update pipelined top testbench

--- Processor/Testbenches/top_tb.sv
+++ Processor/Testbenches/top_tb.sv
@@ -27,69 +27,53 @@ initial begin
 clk = 0;
 reset = 1;
 programFinished = 0;
+cycle = 0;
 
 //initialize input values at byte addresses 0x40 and 0x44
 dut.dataMemory.memory[16] = 32'd7;
 dut.dataMemory.memory[17] = 32'hFFFF_FFFD;
 
-//initialize output at byte address 0x48
+//initialize output location at byte address 0x48
 dut.dataMemory.memory[18] = 32'd0;
 
+//hold reset for two clock edges
 @(posedge clk);
-#1;
+    #1;
+@(posedge clk);
+    #1;
 
 reset = 0;
 
-@(posedge clk);
-#1;
-
-//addi x1, x0, 5 
-if (dut.pc !== 32'd4 || dut.processor.datapath.registerFile.registerArray[1] !== 32'd5)
-$fatal(1,
-    "addi failed! expected pc = 4 and x1 = 5, got pc = %0d and x1 = %0d.", dut.pc, dut.processor.datapath.registerFile.registerArray[1]
-);
-
-if (memoryWrite !== 1'b1 || dataAddress !== 32'd4 || writeData !== 32'd5)
-$fatal(1,
-    "sw signals failed! expected memoryWrite = 1, dataAddress = 4, and writeData = 5, got memoryWrite = %b, dataAddress = %0d, and writeData = %0d.", memoryWrite, dataAddress, writeData
-);
-
-@(posedge clk);
-#1;
-
-//sw x1, 4(x0)
-if (dut.pc !== 32'd8 || dut.dataMemory.memory[1] !== 32'd5)
-$fatal(1,
-    "sw failed! expected pc = 8 and memory[1] = 5, got pc = %0d and memory[1] = %0d.", dut.pc, dut.dataMemory.memory[1]
-);
-
-//run the remaining program
-cycle = 0;
-
+//run program until expected result appears or timeout occurs
 while (cycle < 200 && !programFinished) begin
 
-@(posedge clk);
-#1;
+    @(posedge clk);
+    #1;
 
-if (dut.dataMemory.memory[18] === 32'd5)
-programFinished = 1'b1;
+    if (dut.dataMemory.memory[18] === 32'd5)
+        programFinished = 1'b1;
 
-cycle = cycle + 1;
+    cycle = cycle + 1;
 
 end
 
 if (!programFinished)
-$fatal(1,
-    "Program timed out! expected memory[18] = 5, got memory[18] = %0d.", dut.dataMemory.memory[18]
-);
+    $fatal(1,
+        "Program timed out. expected memory[18] = 5, got memory[18] = %0d.",
+        dut.dataMemory.memory[18]
+    );
 
-if (dut.pc !== 32'd72)
-$fatal(1,
-    "halt failed! expected pc = 72, got pc = %0d.", dut.pc
-);
+// vrify initial addi -> sw sequence eventually stored 5
+if (dut.dataMemory.memory[1] !== 32'd5)
+    $fatal(1,
+        "Initial store failed! expected memory[1] = 5, got memory[1] = %0d.",dut.dataMemory.memory[1]
+    );
 
 $display(
-    "All tests passed successfully. memory[18] = %0d.", dut.dataMemory.memory[18]
+    "All pipelined CPU tests passed. memory[1] = %0d, memory[18] = %0d, cycles = %0d.",
+    dut.dataMemory.memory[1],
+    dut.dataMemory.memory[18],
+    cycle
 );
 
 $finish;
